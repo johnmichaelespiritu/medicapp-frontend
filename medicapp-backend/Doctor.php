057b79c77b111e6ed2fa203d4a7ca7cfd60310df
johnmichaelespiritu <johnmichael.espiritu.pixel8@gmail.com>
2023-07-27 15:13:17 +0800

    Modify and optimize the entire code in order to insert the token and replace the $_SESSION.

--- medicapp-backend/Doctor.php
+++ medicapp-backend/Doctor.php
@@ -2,102 +2,124 @@
 session_start();
 
 require_once('./DB.php');
+require_once('./Utils.php');
 
 class Doctor extends DB
 {
     public function httpGet()
     {
-        $columns = array('u.user_id, d.user_id, d.doctor_id, d.doctor_name, d.specialization, d.contact_number, d.email_address, d.home_address');
+        $token = isset($_GET['token']) ? $_GET['token'] : null;
 
-        if (isset($_GET['searchKeyword'])) {
+        if (!$token) {
+            echo json_encode(array('method' => 'GET', 'status' => 'failed', 'message' => 'Token not provided.'));
+            return;
+        }
 
-            $search_doctor = $this->connection->join('tbl_user_account u', 'u.user_id=d.user_id', 'INNER')->where('d.user_id', $_SESSION['user_id'])->where("(doctor_name like '" . $_GET['searchKeyword'] . "%')")->where('d.deleted_at', null, 'IS')->get('tbl_doctor_information d', null, $columns);
+        try {
+            $decoded_user_id = $this->decodeUserID($_GET['token']);
 
-            if ($search_doctor)
-                echo json_encode(array('method' => 'GET', 'status' => 'success', 'data' => $search_doctor, 'search' => 'doctor'));
-            else 
-                echo json_encode(array('method' => 'GET', 'status' => 'failed', 'data' => $this->connection->getLastError()));
-        } else {
+            if (!$decoded_user_id) {
+                echo json_encode(array('method' => 'GET', 'status' => 'failed', 'message' => 'Invalid token.'));
+                return;
+            }
 
-            $get_all_doctors = $this->connection->join('tbl_user_account u', 'u.user_id=d.user_id', 'INNER')->where('d.user_id', $_SESSION['user_id'])->where('d.deleted_at', null, 'IS')->get('tbl_doctor_information d', null, $columns);
+            $user_id = $decoded_user_id->user_id;
 
-            if ($get_all_doctors)
-                echo json_encode(array('method' => 'GET', 'status' => 'success', 'data' => $get_all_doctors));
-            else
-                echo json_encode(array('method' => 'GET', 'status' => 'failed', 'data' => $this->connection->getLastError()));
+            if (isset($_GET['searchKeyword'])) {
+                $searchKeyword = $_GET['searchKeyword'];
+                $search_doctor = $this->getDoctorInformation($user_id, $searchKeyword);
+
+                if ($search_doctor) 
+                    echo json_encode(array('method' => 'GET', 'status' => 'success', 'data' => $search_doctor, 'search' => 'doctor'));
+                else 
+                    echo json_encode(array('method' => 'GET', 'status' => 'failed', 'data' => $this->connection->getLastError()));
+            } else {
+                $get_all_doctors = $this->getDoctorInformation($user_id);
+                
+                if ($get_all_doctors)
+                    echo json_encode(array('method' => 'GET', 'status' => 'success', 'data' => $get_all_doctors));
+                else 
+                    echo json_encode(array('method' => 'GET', 'status' => 'failed', 'data' => $this->connection->getLastError()));
+            }
+        } catch (Exception $e) {
+            echo json_encode(array('method' => 'GET', 'status' => 'failed', 'message' => 'Invalid token.'));
         }
     }
 
     public function httpPost($payload)
     {
-        if ($this->checkDoctorExistence($payload['doctor_name'], $payload['specialization']))
-            $this->addDoctor($payload);
-        else 
-            echo json_encode(array('method' => 'POST', 'status' => 'failed', 'data' => 'The doctor you entered is already existing.'));
-    }
+        $action = $payload['action'];
 
-    public function httpPut($payload)
-    {
-        if ($this->checkDoctorExistence($payload['doctor_name'], $payload['specialization'], $payload['doctor_id']))
-            $this->updateDoctor($payload);
-        else
-            echo json_encode(array('method' => 'PUT', 'status' => 'failed', 'data' => 'The doctor you entered is already existing.'));
-    }
-
-    public function httpDelete($payload)
-    {
-        $data = array('deleted_at' => date_create()->format('Y-m-d H:i:s'));
-        $delete_doctor = $this->connection->where('doctor_id', $payload['payload'], 'IN')->update('tbl_doctor_information', $data);
-
-        if ($delete_doctor)
-            echo json_encode(array('method' => 'DELETE', 'status' => 'success', 'data' => ''));
-        else
-            echo json_encode(array('method' => 'DELETE', 'status' => 'failed', 'data' => $this->connection->getLastError()));
-    }
-
-    private function checkDoctorExistence($doctor_name, $specialization, $doctor_id = null)
-    {
-        if($doctor_id === null) {
-
-           $search_name_specializations = $this->connection->where('user_id', $_SESSION['user_id'])->where('doctor_name', $doctor_name)->where('specialization', $specialization)->getValue('tbl_doctor_information', 'user_id', null);
-           $search_name_specialization_deleted_at_null = $this->connection->where('user_id', $_SESSION['user_id'])->where('doctor_name', $doctor_name)->where('specialization', $specialization)->where('deleted_at', null, 'IS')->getValue('tbl_doctor_information', 'user_id', null);
-           $search_name_specialization_deleted_at_not_null = $this->connection->where('user_id', $_SESSION['user_id'])->where('doctor_name', $doctor_name)->where('specialization', $specialization)->where('deleted_at', null, 'IS NOT')->getValue('tbl_doctor_information', 'user_id', null);
-
-           if($search_name_specializations === null || ($search_name_specialization_deleted_at_null === null && $search_name_specialization_deleted_at_not_null !== null)) 
-               return true;
-           else
-               return false;
+        switch ($action) {
+            case 'addDoctor':
+                $this->handleAddDoctor($payload);
+                break;
+            case 'updateDoctor':
+                $this->handleUpdateDoctor($payload);
+                break;
+            case 'deleteDoctor':
+                $this->handleDeleteDoctor($payload);
+                break;
+            default:
+                echo json_encode(array('method' => 'POST', 'status' => 'failed', 'message' => 'Unknown action.'));
         }
-        else {
+    }
 
-            $search_name_specializations = $this->connection->where('user_id', $_SESSION['user_id'])->where('doctor_name', $doctor_name)->where('specialization', $specialization)->getValue('tbl_doctor_information', 'user_id', null);
-            $search_name_specialization_deleted_at_null = $this->connection->where('user_id', $_SESSION['user_id'])->where('doctor_name', $doctor_name)->where('specialization', $specialization)->where('deleted_at', null, 'IS')->where('doctor_id', $doctor_id, '=')->getValue('tbl_doctor_information', 'user_id', null);
-            $search_name_specialization_deleted_at_not_null = $this->connection->where('user_id', $_SESSION['user_id'])->where('doctor_name', $doctor_name)->where('specialization', $specialization)->where('deleted_at', null, 'IS NOT')->where('doctor_id', $doctor_id, '!=')->getValue('tbl_doctor_information', 'user_id', null);
-    
-            if($search_name_specializations === null || ($search_name_specialization_deleted_at_null !== null || $search_name_specialization_deleted_at_not_null !== null)) 
-                return true;
-            else
-                return false;
+    private function handleAddDoctor($payload)
+    {
+        if ($this->checkDoctorExistence($payload['doctor_name'], $payload['specialization'])) {
+            $this->addDoctor($payload);
+        } else {
+            echo json_encode(array('method' => 'POST', 'status' => 'failed', 'message' => 'The doctor you entered is already existing.'));
         }
     }
 
     private function addDoctor($payload)
     {
-        $data = array(
-            'user_id' => $_SESSION['user_id'],
-            'doctor_name' => $payload['doctor_name'],
-            'specialization' => $payload['specialization'],
-            'contact_number' => $payload['contact_number'],
-            'email_address' => $payload['email_address'],
-            'home_address' => $payload['home_address']
-        );
+        $token = isset($_GET['token']) ? $_GET['token'] : null;
 
-        $add_doctor = $this->connection->insert('tbl_doctor_information', $data);
+        if (!$token) {
+            echo json_encode(array('method' => 'GET', 'status' => 'failed', 'message' => 'Token not provided.'));
+            return;
+        }
 
-        if ($add_doctor)
-            echo json_encode(array('method' => 'POST', 'status' => 'success', 'data' => $data));
-        else
-            echo json_encode(array('method' => 'POST', 'status' => 'failed', 'data' => 'Doctor added unsuccessfully.'));
+        try {
+            $decoded_user_id = $this->decodeUserID($_GET['token']);
+
+            if (!$decoded_user_id) {
+                echo json_encode(array('method' => 'GET', 'status' => 'failed', 'message' => 'Invalid token.'));
+                return;
+            }
+
+            $user_id = $decoded_user_id->user_id;
+
+            $data = array(
+                'user_id' => $user_id,
+                'doctor_name' => $payload['doctor_name'],
+                'specialization' => $payload['specialization'],
+                'contact_number' => $payload['contact_number'],
+                'email_address' => $payload['email_address'],
+                'home_address' => $payload['home_address']
+            );
+
+            $add_doctor = $this->connection->insert('tbl_doctor_information', $data);
+
+            if ($add_doctor)
+                echo json_encode(array('method' => 'POST', 'status' => 'success', 'data' => $data, 'message' => 'Doctor added successfully.'));
+            else
+                echo json_encode(array('method' => 'POST', 'status' => 'failed', 'message' => 'Doctor added unsuccessfully.'));
+        } catch (Exception $e) {
+            echo json_encode(array('method' => 'GET', 'status' => 'failed', 'message' => 'Invalid token.'));
+        }
+    }
+
+    private function handleUpdateDoctor($payload)
+    {
+        if ($this->checkDoctorExistence($payload['doctor_name'], $payload['specialization'], $payload['doctor_id'])) {
+            $this->updateDoctor($payload);
+        } else {
+            echo json_encode(array('method' => 'PUT', 'status' => 'failed', 'message' => 'The doctor you entered is already existing.'));
+        }
     }
 
     private function updateDoctor($payload)
@@ -113,9 +135,89 @@ class Doctor extends DB
         $update_doctor = $this->connection->where('doctor_id', $payload['doctor_id'])->update('tbl_doctor_information', $data);
 
         if ($update_doctor)
-            echo json_encode(array('method' => 'PUT', 'status' => 'success', 'data' => 'Doctor information updated successfully.'));
+            echo json_encode(array('method' => 'PUT', 'status' => 'success', 'message' => 'Doctor information updated successfully.'));
         else
-            echo json_encode(array('method' => 'PUT', 'status' => 'failed', 'data' => 'Doctor information updated unsuccessfully.'));
+            echo json_encode(array('method' => 'PUT', 'status' => 'failed', 'message' => 'Doctor information updated unsuccessfully.'));
+    }
+
+    private function handleDeleteDoctor($payload)
+    {
+        $data = array('deleted_at' => date_create()->format('Y-m-d H:i:s'));
+        $delete_doctor = $this->connection->where('doctor_id', $payload['doctor_ids'], 'IN')->update('tbl_doctor_information', $data);
+
+        if ($delete_doctor) {
+            echo json_encode(array('method' => 'POST', 'status' => 'success', 'message' => 'Doctor deleted successfully.'));
+        } else {
+            echo json_encode(array('method' => 'POST', 'status' => 'failed', 'message' => 'Doctor deleted unsuccessfully.'));
+        }
+    }
+
+    private function decodeUserID($token) 
+    {
+        $token_decoder = new Token();
+        $decoded_token = $token_decoder->decodeToken($token);
+
+        return $decoded_token;
+    }
+
+    private function getDoctorInformation($user_id, $search_keyword = null)
+    {
+        $columns = array('u.user_id, d.user_id, d.doctor_id, d.doctor_name, d.specialization, d.contact_number, d.email_address, d.home_address');
+
+        $query = $this->connection->join('tbl_user_account u', 'u.user_id=d.user_id', 'INNER')->where('d.user_id', $user_id)->where('d.deleted_at', null, 'IS');
+
+        if ($search_keyword !== null) {
+            $query->where("(doctor_name like '" . $this->connection->escape($search_keyword) . "%')");
+        }
+
+        $getDoctorInformationQuery = $query->get('tbl_doctor_information d', null, $columns);
+
+        return $getDoctorInformationQuery;
+    }
+
+    private function checkDoctorExistence($doctor_name, $specialization, $doctor_id = null)
+    {
+        $token = isset($_GET['token']) ? $_GET['token'] : null;
+
+        if (!$token) {
+            echo json_encode(array('method' => 'GET', 'status' => 'failed', 'message' => 'Token not provided.'));
+            return;
+        }
+
+        try {
+            $decoded_user_id = $this->decodeUserID($_GET['token']);
+
+            if (!$decoded_user_id) {
+                echo json_encode(array('method' => 'GET', 'status' => 'failed', 'message' => 'Invalid token.'));
+                return;
+            }
+
+            $user_id = $decoded_user_id->user_id;
+
+            if($doctor_id === null) {
+
+               $search_name_specializations = $this->connection->where('user_id', $user_id)->where('doctor_name', $doctor_name)->where('specialization', $specialization)->getValue('tbl_doctor_information', 'user_id', null);
+               $search_name_specialization_deleted_at_null = $this->connection->where('user_id', $user_id)->where('doctor_name', $doctor_name)->where('specialization', $specialization)->where('deleted_at', null, 'IS')->getValue('tbl_doctor_information', 'user_id', null);
+               $search_name_specialization_deleted_at_not_null = $this->connection->where('user_id', $user_id)->where('doctor_name', $doctor_name)->where('specialization', $specialization)->where('deleted_at', null, 'IS NOT')->getValue('tbl_doctor_information', 'user_id', null);
+
+               if($search_name_specializations === null || ($search_name_specialization_deleted_at_null === null && $search_name_specialization_deleted_at_not_null !== null)) 
+                   return true;
+               else
+                   return false;
+            } else {
+
+                $search_name_specializations = $this->connection->where('user_id', $user_id)->where('doctor_name', $doctor_name)->where('specialization', $specialization)->getValue('tbl_doctor_information', 'user_id', null);
+                $search_name_specialization_deleted_at_null = $this->connection->where('user_id', $user_id)->where('doctor_name', $doctor_name)->where('specialization', $specialization)->where('deleted_at', null, 'IS')->where('doctor_id', $doctor_id, '=')->getValue('tbl_doctor_information', 'user_id', null);
+                $search_name_specialization_deleted_at_not_null = $this->connection->where('user_id', $user_id)->where('doctor_name', $doctor_name)->where('specialization', $specialization)->where('deleted_at', null, 'IS NOT')->where('doctor_id', $doctor_id, '!=')->getValue('tbl_doctor_information', 'user_id', null);
+        
+                if($search_name_specializations === null || ($search_name_specialization_deleted_at_null !== null || $search_name_specialization_deleted_at_not_null !== null)) 
+                    return true;
+                else
+                    return false;
+            }
+        } catch (Exception $e) {
+            echo json_encode(array('method' => 'GET', 'status' => 'failed', 'message' => 'Invalid token.'));
+        }
     }
 }
 
@@ -126,17 +228,9 @@ $request_method = $_SERVER['REQUEST_METHOD'];
 $doctor = new Doctor;
 
 if ($request_method === 'GET') {
-    $doctor->httpGet($received_data);
+    $doctor->httpGet();
 }
 
 if ($request_method === 'POST') {
     $doctor->httpPost($received_data);
-}
-
-if ($request_method === 'PUT') {
-    $doctor->httpPut($received_data);
-}
-
-if ($request_method === 'DELETE') {
-    $doctor->httpDelete($received_data);
 }
