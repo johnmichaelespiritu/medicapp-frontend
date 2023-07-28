77baa394359e50da5372ebbdef8ab7146c02d0d6
johnmichaelespiritu <johnmichael.espiritu.pixel8@gmail.com>
2023-07-28 19:38:33 +0800

    Convert the put and delete request method into post method.

--- medicapp-backend/Patient.php
+++ medicapp-backend/Patient.php
@@ -1,55 +1,110 @@
 <?php
-session_start();
 
 require_once('./DB.php');
+require_once('./Utils.php');
 
 class Patient extends DB
 {
     public function httpGet()
     {
-        $columns = array('u.user_id, p.user_id, p.patient_id, p.patient_name, p.patient_age, p.patient_home_address, p.patient_contact_number, p.patient_gender, p.patient_birthdate');
+        $token = isset($_GET['token']) ? $_GET['token'] : null;
 
-        if (isset($_GET['searchKeyword'])) {
+        if (!$token) {
+            echo json_encode(array('method' => 'GET', 'status' => 'failed', 'message' => 'Token not provided.'));
+            return;
+        }
 
-            $search_patient = $this->connection->join('tbl_user_account u', 'u.user_id=p.user_id', 'INNER')->where('p.user_id', $_SESSION['user_id'])->where("(patient_name like '" . $_GET['searchKeyword'] . "%')")->where('p.patient_deleted_at', null, 'IS')->get('tbl_patient_information p', null, $columns);
+        try {
+            $decoded_user_id = $this->decodeUserID($_GET['token']);
 
-            if ($search_patient) {
-                echo json_encode(array('method' => 'GET', 'status' => 'success', 'data' => $search_patient));
-            } else {
-                echo json_encode(array('method' => 'GET', 'status' => 'failed', 'data' => $this->connection->getLastError()));
+            if (!$decoded_user_id) {
+                echo json_encode(array('method' => 'GET', 'status' => 'failed', 'message' => 'Invalid token.'));
+                return;
             }
-        } else {
 
-            $get_all_patients = $this->connection->join('tbl_user_account u', 'u.user_id=p.user_id', 'INNER')->where('p.user_id', $_SESSION['user_id'])->where('p.patient_deleted_at', null, 'IS')->get('tbl_patient_information p', null, $columns);
+            $user_id = $decoded_user_id->user_id;
 
-            if ($get_all_patients)
-                echo json_encode(array('method' => 'GET', 'status' => 'success', 'data' => $get_all_patients));
-            else
-                echo json_encode(array('method' => 'GET', 'status' => 'failed', 'data' => $this->connection->getLastError()));
+            if (isset($_GET['searchKeyword'])) {
+                $searchKeyword = $_GET['searchKeyword'];
+                $search_patient = $this->getPatientInformation($user_id, $searchKeyword);
+
+                if ($search_patient) 
+                    echo json_encode(array('method' => 'GET', 'status' => 'success', 'data' => $search_patient));
+                else 
+                    echo json_encode(array('method' => 'GET', 'status' => 'failed', 'data' => $this->connection->getLastError()));
+            } else {
+                $get_all_patients = $this->getPatientInformation($user_id);
+                
+                if ($get_all_patients)
+                    echo json_encode(array('method' => 'GET', 'status' => 'success', 'data' => $get_all_patients));
+                else 
+                    echo json_encode(array('method' => 'GET', 'status' => 'failed', 'data' => $this->connection->getLastError()));
+            }
+        } catch (Exception $e) {
+            echo json_encode(array('method' => 'GET', 'status' => 'failed', 'message' => 'Invalid token.'));
         }
     }
 
     public function httpPost($payload)
     {
-        $data = array(
-            'user_id' => $_SESSION['user_id'],
-            'patient_name' => $payload['patient_name'],
-            'patient_age' => $payload['patient_age'],
-            'patient_home_address' => $payload['patient_home_address'],
-            'patient_contact_number' => $payload['patient_contact_number'],
-            'patient_gender' => $payload['patient_gender'],
-            'patient_birthdate' => $payload['patient_birthdate']
-        );
+        $action = $payload['action'];
 
-        $add_patient = $this->connection->insert('tbl_patient_information', $data);
-
-        if ($add_patient)
-            echo json_encode(array('method' => 'POST', 'status' => 'success', 'data' => $data));
-        else
-            echo json_encode(array('method' => 'POST', 'status' => 'failed', 'data' => 'Patient added unsuccessfully.'));
+        switch ($action) {
+            case 'addPatient':
+                $this->addPatient($payload);
+                break;
+            case 'updatePatient':
+                $this->updatePatient($payload);
+                break;
+            case 'deletePatient':
+                $this->deletePatient($payload);
+                break;
+            default:
+                echo json_encode(array('method' => 'POST', 'status' => 'failed', 'message' => 'Unknown action.'));
+        }
     }
 
-    public function httpPut($payload)
+    private function addPatient($payload) 
+    {
+        $token = isset($_GET['token']) ? $_GET['token'] : null;
+
+        if (!$token) {
+            echo json_encode(array('method' => 'GET', 'status' => 'failed', 'message' => 'Token not provided.'));
+            return;
+        }
+
+        try {
+            $decoded_user_id = $this->decodeUserID($_GET['token']);
+
+            if (!$decoded_user_id) {
+                echo json_encode(array('method' => 'GET', 'status' => 'failed', 'message' => 'Invalid token.'));
+                return;
+            }
+
+            $user_id = $decoded_user_id->user_id;
+
+             $data = array(
+                'user_id' => $user_id,
+                'patient_name' => $payload['patient_name'],
+                'patient_age' => $payload['patient_age'],
+                'patient_home_address' => $payload['patient_home_address'],
+                'patient_contact_number' => $payload['patient_contact_number'],
+                'patient_gender' => $payload['patient_gender'],
+                'patient_birthdate' => $payload['patient_birthdate']
+            );
+
+            $add_patient = $this->connection->insert('tbl_patient_information', $data);
+
+            if ($add_patient)
+                echo json_encode(array('method' => 'POST', 'status' => 'success', 'data' => $data, 'message' => 'Patient added successfully.'));
+            else
+                echo json_encode(array('method' => 'POST', 'status' => 'failed', 'message' => 'Patient added unsuccessfully.'));
+        } catch (Exception $e) {
+            echo json_encode(array('method' => 'GET', 'status' => 'failed', 'message' => 'Invalid token.'));
+        }
+    }
+
+    private function updatePatient($payload) 
     {
         $data = array(
             'patient_name' => $payload['patient_name'],
@@ -63,20 +118,43 @@ class Patient extends DB
         $update_patient = $this->connection->where('patient_id', $payload['patient_id'])->update('tbl_patient_information', $data);
 
         if ($update_patient)
-            echo json_encode(array('method' => 'PUT', 'status' => 'success', 'data' => 'Patient information updated successfully.'));
+            echo json_encode(array('method' => 'POST', 'status' => 'success', 'message' => 'Patient information updated successfully.'));
         else
-            echo json_encode(array('method' => 'PUT', 'status' => 'failed', 'data' => 'Patient information updated unsuccessfully.'));
+            echo json_encode(array('method' => 'POST', 'status' => 'failed', 'message' => 'Patient information updated unsuccessfully.'));
     }
 
-    public function httpDelete($payload)
+    private function deletePatient($payload) 
     {
         $data = array('patient_deleted_at' => date_create()->format('Y-m-d H:i:s'));
-        $delete_patient = $this->connection->where('patient_id', $payload['payload'], 'IN')->update('tbl_patient_information', $data);
+        $delete_patient = $this->connection->where('patient_id', $payload['ids'], 'IN')->update('tbl_patient_information', $data);
 
         if ($delete_patient)
-            echo json_encode(array('method' => 'DELETE', 'status' => 'success', 'data' => ''));
+            echo json_encode(array('method' => 'POST', 'status' => 'success', 'message' => 'Patient deleted successfully.'));
         else
-            echo json_encode(array('method' => 'DELETE', 'status' => 'failed', 'data' => $this->connection->getLastError()));
+            echo json_encode(array('method' => 'POST', 'status' => 'failed', 'message' => 'Patient deleted unsuccessfully.'));
+    }
+
+    private function decodeUserID($token) 
+    {
+        $token_decoder = new Token();
+        $decoded_token = $token_decoder->decodeToken($token);
+
+        return $decoded_token;
+    }
+
+    private function getPatientInformation($user_id, $search_keyword = null)
+    {
+        $columns = array('u.user_id, p.user_id, p.patient_id, p.patient_name, p.patient_age, p.patient_home_address, p.patient_contact_number, p.patient_gender, p.patient_birthdate');
+    
+        $query = $this->connection->join('tbl_user_account u', 'u.user_id=p.user_id', 'INNER')->where('p.user_id', $user_id)->where('p.patient_deleted_at', null, 'IS');
+    
+        if ($search_keyword !== null) {
+            $query->where("(patient_name like '" . $this->connection->escape($search_keyword) . "%')");
+        }
+    
+        $getPatientInformationQuery = $query->get('tbl_patient_information p', null, $columns);
+    
+        return $getPatientInformationQuery;
     }
 }
 
@@ -87,17 +165,9 @@ $request_method = $_SERVER['REQUEST_METHOD'];
 $patient = new Patient;
 
 if ($request_method === 'GET') {
-    $patient->httpGet($received_data);
+    $patient->httpGet();
 }
 
 if ($request_method === 'POST') {
     $patient->httpPost($received_data);
-}
-
-if ($request_method === 'PUT') {
-    $patient->httpPut($received_data);
-}
-
-if ($request_method === 'DELETE') {
-    $patient->httpDelete($received_data);
 }
