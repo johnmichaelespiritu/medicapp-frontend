cc6a2bd86fc05db28f2c0281ff0f45bedce50f90
johnmichaelespiritu <johnmichael.espiritu.pixel8@gmail.com>
2023-07-29 15:09:16 +0800

    Add documentation comments.

--- src/composables/Medicapp.js
+++ src/composables/Medicapp.js
@@ -1,13 +1,15 @@
 import { ref, readonly } from "vue";
 
-// const baseBackendURL =
-//   "http://localhost/medicapp/medicapp-frontend/medicapp-backend/";
+// Base URL for the backend API.
 const baseBackendURL =
   "https://medicappsystem.000webhostapp.com/medicapp-backend/";
+
 export const searchContents = ref([]);
+
+// Authentication token retrieved from localStorage.
 const token = JSON.parse(localStorage.getItem("token"));
 
-//Sign Up
+// User Account State
 export const userAccount = ref([]);
 export const searchUserName = ref([]);
 export const userAccountList = readonly(userAccount);
@@ -15,20 +17,20 @@ export const userID = ref(0);
 export const userEmail = ref(null);
 export const userEmailVerificationPurpose = ref(null);
 
-//Doctor
+// Doctor State
 export const doctor = ref([]);
 export const doctorLists = readonly(doctor);
 export const doctorInformation = ref([]);
 export const specificDoctorInformation = readonly(doctorInformation);
 export const searchDoctorContents = ref([]);
 
-//Patient
+// Patient State
 export const patient = ref([]);
 export const patientLists = readonly(patient);
 export const patientInformation = ref([]);
 export const specificPatientInformation = readonly(patientInformation);
 
-//Consultation
+// Consultation State
 export const consultation = ref([]);
 export const consultationLists = readonly(consultation);
 export const consultationInformation = ref([]);
@@ -36,7 +38,7 @@ export const specificConsultationInformation = readonly(
   consultationInformation
 );
 
-//Triggers
+// Triggers State
 export const trigger = ref({
   activeMenu: "",
   leftDrawerOpen: false,
@@ -53,158 +55,297 @@ export const trigger = ref({
   showEmailVerificationForm: false,
 });
 
-// User Account Functions
+/**
+ * loginFunction
+ *
+ * Performs a login request to the backend API.
+ *
+ * @param {Object} payload - The payload containing login credentials.
+ * @returns {Promise} A Promise that resolves with the response data if the login is successful, otherwise rejects with an error.
+ */
 export const loginFunction = (payload) => {
+  // Create a Promise that will perform the API request.
   return new Promise((resolve, reject) => {
+    // Send a POST request to the backend API with login credentials in the payload.
     fetch(`${baseBackendURL}Login.php`, {
       method: "POST",
       body: JSON.stringify(payload),
       credentials: "include",
     })
+      // Parse the response data as JSON.
       .then((response) => response.json())
       .then((data) => {
+        // If login is successful, store the user token in the localStorage.
         if (data.status === "success") {
           localStorage.setItem("token", JSON.stringify(data.data));
         }
+        // Resolve the Promise with the response data.
         resolve(data);
       })
       .catch((error) => {
+        // Reject the Promise with an error if there's an issue with the request.
         reject(error);
       });
   });
 };
 
+/**
+ * logoutFunction
+ *
+ * Performs a logout request to the backend API.
+ *
+ * @param {Object} payload - The payload for logout.
+ * @returns {Promise} A Promise that resolves with the response data if the logout is successful, otherwise rejects with an error.
+ */
 export const logoutFunction = (payload) => {
+  // Create a Promise that will perform the API request.
   return new Promise((resolve, reject) => {
+    // Send a POST request to the backend API with the payload for logout.
     fetch(`${baseBackendURL}Login.php`, {
       method: "POST",
       body: JSON.stringify(payload),
       credentials: "include",
     })
+      // Parse the response data as JSON.
       .then((response) => response.json())
       .then((data) => {
+        // Resolve the Promise with the response data.
         resolve(data);
       })
       .catch((error) => {
+        // Reject the Promise with an error if there's an issue with the request.
         reject(error);
       });
   });
 };
 
+/**
+ * updateFunction
+ *
+ * Performs an update request to the backend API.
+ *
+ * @param {Object} payload - The payload containing data to be updated.
+ * @returns {Promise} A Promise that resolves with the response data if the update is successful, otherwise rejects with an error.
+ */
 export const updateFunction = (payload) => {
+  // Create a Promise that will perform the API request.
   return new Promise((resolve, reject) => {
+    // Send a POST request to the backend API with the payload for update.
     fetch(`${baseBackendURL}Login.php`, {
       method: "POST",
       body: JSON.stringify(payload),
     })
+      // Parse the response data as JSON.
       .then((response) => response.json())
       .then((data) => {
+        // Resolve the Promise with the response data.
         resolve(data);
       })
       .catch((error) => {
+        // Reject the Promise with an error if there's an issue with the request.
         reject(error);
       });
   });
 };
 
+/**
+ * getAllDataList
+ *
+ * Retrieves all data from the backend API based on the specified path and stores it in the 'information' variable.
+ *
+ * @param {string} path - The path to the backend API endpoint.
+ * @param {object} information - The object where the retrieved data will be stored.
+ * @returns {Promise} A Promise that resolves with the response data if the request is successful, otherwise rejects with an error.
+ */
 export const getAllDataList = (path, information) => {
+  // Create a Promise that will perform the API request.
   return new Promise((resolve, reject) => {
+    // Send a GET request to the backend API with the provided path and token.
     fetch(`${baseBackendURL}${path}?token=${encodeURIComponent(token.token)}`, {
       method: "GET",
     })
+      // Parse the response data as JSON.
       .then((response) => response.json())
       .then((data) => {
+        // Check if the response status is 'success'.
         if (data.status === "success") {
+          // If successful, store the retrieved data in the 'information' variable.
           information.value = data.data;
         }
+        // Resolve the Promise with the response data.
         resolve(data);
       })
       .catch((error) => {
+        // Reject the Promise with an error if there's an issue with the request.
         reject(error);
       });
   });
 };
 
+/**
+ * getSpecificInformation
+ *
+ * Retrieves specific information from the 'data' object based on the provided payload and stores it in the 'result' variable.
+ *
+ * @param {object} data - The object containing the data to search in.
+ * @param {string} payload - The value to be searched for in the 'data' object.
+ * @param {string} id - The key to use for comparison with the 'payload'.
+ * @param {object} result - The object where the matched information will be stored.
+ */
 export const getSpecificInformation = (data, payload, id, result) => {
+  // Initialize the 'result' variable as an empty array to store the matched information
   result.value = [];
+
+  // Find the index of the object in 'data.value' that matches the 'payload' using the 'id' key.
   let objectIndex = data.value.findIndex((e) => e[id] === payload);
+
+  // Check if the object is found in 'data.value' based on the 'id' comparison.
   if (objectIndex !== -1) {
+    // If a matching object is found, push it to the 'result' array
     result.value.push(data.value[objectIndex]);
   }
 };
 
+/**
+ * getSearchResult
+ *
+ * Performs a search request to the backend API based on the provided payload and returns the search results.
+ *
+ * @param {object} payload - The payload containing the search parameters and path.
+ * @returns {Promise} A Promise that resolves with the response data if the search is successful, otherwise rejects with an error.
+ */
 const getSearchResult = (payload) => {
+  // Convert the search parameters in 'payload.params' to a URL-encoded string.
   const queryParams = new URLSearchParams(payload.params).toString();
+
+  // Combine the base URL path and the URL-encoded search parameters to form the complete URL.
   const url = `${payload.path}&${queryParams}`;
 
+  // Create a Promise that will perform the API request.
   return new Promise((resolve, reject) => {
+    // Send a GET request to the backend API with the generated URL.
     fetch(url, {
       method: "GET",
     })
+      // Parse the response data as JSON.
       .then((response) => response.json())
       .then((data) => {
+        // Resolve the Promise with the response data.
         resolve(data);
       })
       .catch((error) => {
+        // Reject the Promise with an error if there's an issue with the request.
         reject(error);
       });
   });
 };
 
+/**
+ * filterData
+ *
+ * Filters data from the backend API based on the search 'val' and updates the 'searchDoctorContents' or 'searchContents' variables.
+ *
+ * @param {string} path - The path to the backend API endpoint.
+ * @param {string} val - The value to be used for filtering the data.
+ * @param {function} update - The function used for updating the 'searchDoctorContents' or 'searchContents' variable.
+ * @param {function} abort - The function used for aborting the update.
+ */
 export const filterData = (path, val, update, abort) => {
+  // Check if the search value 'val' is not empty.
   if (val.length > 0) {
+    // Create a payload object with the search path and parameters.
     let payload = {
       path: `${baseBackendURL}${path}?token=${encodeURIComponent(token.token)}`,
       params: {
         searchKeyword: val,
       },
     };
+
+    // Call the 'update' function to initiate the update of 'searchDoctorContents' or 'searchContents'.
     update(() => {
+      // Perform the search using 'getSearchResult' function with the generated payload.
       getSearchResult(payload).then((data) => {
+        // Check if the search result is for "doctor".
         if (data.search === "doctor") {
+          // If the search is for "doctor", update 'searchDoctorContents' variable with the search data.
           searchDoctorContents.value = data.data;
         } else {
+          // Otherwise, update 'searchContents' variable with the search data.
           searchContents.value = data.data;
         }
       });
     });
   } else {
+    // If the search value 'val' is empty, call the 'abort' function to abort the update.
     abort();
   }
 };
 
+/**
+ * addData
+ *
+ * Performs an add request to the backend API and adds the new data to the 'information' variable.
+ *
+ * @param {string} path - The path to the backend API endpoint.
+ * @param {object} payload - The payload containing the data to be added.
+ * @param {object} information - The object where the new data will be added.
+ * @returns {Promise} A Promise that resolves with the response data if the addition is successful, otherwise rejects with an error.
+ */
 export const addData = (path, payload, information) => {
+  // Create a Promise that will perform the API request.
   return new Promise((resolve, reject) => {
+    // Perform a POST request to the backend API with the provided payload.
     fetch(`${baseBackendURL}${path}?token=${encodeURIComponent(token.token)}`, {
       method: "POST",
       body: JSON.stringify(payload),
     })
+      // Parse the response as JSON.
       .then((response) => response.json())
       .then((data) => {
+        // Check if the API response indicates a successful addition.
         if (data.status === "success") {
+          // Add the new data to the 'information' variable.
           information.value.push(data.data);
         }
+        // Resolve the Promise with the response data.
         resolve(data);
       })
       .catch((error) => {
+        // Reject the Promise with an error if there's an issue with the request.
         reject(error);
       });
   });
 };
 
+/**
+ * updateData
+ *
+ * Performs an update request to the backend API and updates the 'information' variable with the modified data.
+ *
+ * @param {string} path - The path to the backend API endpoint.
+ * @param {object} payload - The payload containing the updated data.
+ * @param {object} information - The object where the updated data will be stored.
+ * @param {string} id - The key used for finding the object to update in the 'information' variable.
+ * @returns {Promise} A Promise that resolves with the response data if the update is successful, otherwise rejects with an error.
+ */
 export const updateData = (path, payload, information, id) => {
+  // Create a Promise that will perform the API request.
   return new Promise((resolve, reject) => {
+    // Perform a POST request to the backend API with the provided payload.
     fetch(`${baseBackendURL}${path}?token=${encodeURIComponent(token.token)}`, {
       method: "POST",
       body: JSON.stringify(payload),
     })
+      // Parse the response as JSON.
       .then((response) => response.json())
       .then((data) => {
+        // Check if the API response indicates a successful update.
         if (data.status === "success") {
+          // Find the index of the object to update in the 'information' variable.
           let objectIndex = information.value.findIndex(
             (e) => e[id] === payload[id]
           );
-          // if index not found (-1) update nothing !
+          // If index is found (not -1), update the object properties with the payload values.
           if (objectIndex !== -1) {
             Object.keys(information.value[objectIndex]).forEach((key) => {
               payload[key] &&
@@ -212,34 +353,54 @@ export const updateData = (path, payload, information, id) => {
             });
           }
         }
+        // Resolve the Promise with the response data.
         resolve(data);
       })
       .catch((error) => {
+        // Reject the Promise with an error if there's an issue with the request.
         reject(error);
       });
   });
 };
 
+/**
+ * deleteData
+ *
+ * Performs a delete request to the backend API and removes the deleted data from the 'information' variable.
+ *
+ * @param {string} path - The path to the backend API endpoint.
+ * @param {object} payload - The payload containing the IDs of the data to be deleted.
+ * @param {object} information - The object where the data will be deleted from.
+ * @param {string} id - The key used for finding the objects to delete in the 'information' variable.
+ * @returns {Promise} A Promise that resolves with the response data if the deletion is successful, otherwise rejects with an error.
+ */
 export const deleteData = (path, payload, information, id) => {
+  // Create a Promise that will perform the API request.
   return new Promise((resolve, reject) => {
+    // Perform a POST request to the backend API with the provided payload.
     fetch(`${baseBackendURL}${path}?token=${encodeURIComponent(token.token)}`, {
       method: "POST",
       body: JSON.stringify(payload),
     })
+      // Parse the response as JSON.
       .then((response) => response.json())
       .then((data) => {
+        // Check if the API response indicates a successful deletion.
         if (data.status === "success") {
+          // Loop through the IDs in the payload and find the matching object index in 'information'.
           payload.ids.forEach((ids) => {
             let objectIndex = information.value.findIndex((e) => e[id] === ids);
-            // if index not found (-1) delete nothing !
+            // If index is found (not -1), remove the object from the 'information' array.
             if (objectIndex !== -1) {
               information.value.splice(objectIndex, 1);
             }
           });
         }
+        // Resolve the Promise with the response data.
         resolve(data);
       })
       .catch((error) => {
+        // Reject the Promise with an error if there's an issue with the request.
         reject(error);
       });
   });
--- src/composables/Utils.js
+++ src/composables/Utils.js
@@ -1,7 +1,9 @@
 import { computed } from "vue";
 
+// An array of strings representing gender options available in the application.
 export const genderOptions = ["Male", "Female", "Others"];
 
+// An array of strings representing different statuses for consultations in the application.
 export const statuses = [
   "Scheduled",
   "In Progress",
@@ -12,32 +14,84 @@ export const statuses = [
   "Follow-up",
 ];
 
+/**
+ * rules
+ *
+ * An array of validation rules used for form fields in the application.
+ * The validation rule checks if a value exists (not null and not empty).
+ *
+ * @type {Array<Function>}
+ */
 export const rules = [(val) => (val && val.length > 0) || ""];
 
+/**
+ * validateFields
+ *
+ * Returns a validation function for a specific field.
+ *
+ * @param {Object} field - The field object with a 'label' property representing the field's label.
+ * @returns {Function} A validation function that checks if the field value exists.
+ */
 export const validateFields = (field) => {
+  // Return a validation function for the provided field.
   return (val) =>
     (val !== null && val !== "") || `Please input the ${field.label}.`;
 };
 
+/**
+ * validateFormInputs
+ *
+ * Validates the form inputs based on the provided fields.
+ *
+ * @param {Object} form - The form object containing form data as key-value pairs.
+ * @param {Array<Object>} fields - An array of field objects used for validation.
+ * @returns {boolean} True if all fields have non-empty values, false otherwise.
+ */
 export const validateFormInputs = (form, fields) => {
+  // Check if every field in the 'fields' array has a non-empty value in the 'form' object.
   return fields.every((field) => {
     let value = form.value[field.key];
     return value && value.length > 0;
   });
 };
 
+/**
+ * compareInformation
+ *
+ * Compares the form data with the specificInformation data for specified fields.
+ *
+ * @param {Object} form - The form object containing form data as key-value pairs.
+ * @param {Array<Object>} fields - An array of field objects used for comparison.
+ * @param {Object} specificInformation - The specific information object to compare with.
+ * @returns {boolean} True if any field in the form data is different from the specificInformation data, false otherwise.
+ */
 export const compareInformation = (form, fields, specificInformation) => {
   return fields.some((field) => {
     return form.value[field.key] !== specificInformation.value[0][field.key];
   });
 };
 
+/**
+ * resetForm
+ *
+ * Resets the form fields by setting their values to null.
+ *
+ * @param {Object} form - The form object containing form data as key-value pairs.
+ */
 export const resetForm = (form) => {
+  // Loop through all fields in the 'form' object and set their values to null.
   for (let fields in form) {
     form[fields] = null;
   }
 };
 
+/**
+ * validateAge
+ *
+ * Prevents non-numeric keys from being entered in an input field to validate age.
+ *
+ * @param {Event} event - The keydown event object.
+ */
 export const validateAge = (event) => {
   const key = event.key;
   const allowedKeys = [
@@ -54,12 +108,24 @@ export const validateAge = (event) => {
     "Backspace",
     "Delete",
   ];
+  // If the pressed key is not included in the allowedKeys array, prevent its default action (typing in the input field).
   if (!allowedKeys.includes(key)) {
     event.preventDefault();
   }
 };
 
+/**
+ * showNotification
+ *
+ * Displays a notification using Quasar's notify feature.
+ *
+ * @param {object} $quasar - The Quasar framework instance.
+ * @param {string} type - The type of notification ('positive', 'negative', 'warning', 'info').
+ * @param {string} message - The message to be displayed in the notification.
+ * @param {number} timeout - The timeout duration for the notification to be shown.
+ */
 export const showNotification = ($quasar, type, message, timeout) => {
+  // Call Quasar's notify function with the provided parameters to display the notification.
   $quasar.notify({
     type,
     message,
@@ -67,21 +133,30 @@ export const showNotification = ($quasar, type, message, timeout) => {
   });
 };
 
-export const getSelectedRecord = (deleteMultipleValue, recordLists) => {
-  return deleteMultipleValue.value.length === 0
-    ? ""
-    : `${deleteMultipleValue.value.length} record${
-        deleteMultipleValue.value.length > 1 ? "s" : ""
-      } selected of ${recordLists.value.length}`;
-};
-
+/**
+ * useSelectedRecords
+ *
+ * Provides a function to get the message for the number of selected records and the total number of records in a list.
+ *
+ * @param {ref} deleteMultipleValue - The ref containing the array of selected records.
+ * @param {ref} recordLists - The ref containing the array of all records in the list.
+ * @returns {Object} An object with a function 'getSelectedRecords' to get the message.
+ */
 export const useSelectedRecords = (deleteMultipleValue, recordLists) => {
+  /**
+   * getSelectedRecords
+   *
+   * Gets the message for the number of selected records and the total number of records in a list.
+   *
+   * @returns {string} A message indicating the number of selected records and the total number of records.
+   */
   const getSelectedRecords = () => {
     const recordSelectedCount = deleteMultipleValue.value.length;
     const recordCount = recordLists.value.length;
 
     let message = "";
 
+    // Check if there are any selected records. If so, construct a message indicating the number of selected records and the total number of records in the list.
     if (recordSelectedCount > 0) {
       message = `${recordSelectedCount} record${
         recordSelectedCount > 1 ? "s" : ""
@@ -96,14 +171,26 @@ export const useSelectedRecords = (deleteMultipleValue, recordLists) => {
   };
 };
 
+/**
+ * useListPage
+ *
+ * Filters and returns the list of records based on specific information and search value.
+ *
+ * @param {ref} specificInformation - The ref containing specific information about the records.
+ * @param {ref} searchValue - The ref containing the search value.
+ * @param {ref} listValue - The ref containing the full list of records.
+ * @returns {Object} An object with a computed property 'listPage' representing the filtered list of records.
+ */
 export const useListPage = (specificInformation, searchValue, listValue) => {
   const listPage = computed(() => {
     let list = [];
 
+    // If there is specific information available, display it on the list page.
     if (specificInformation.value.length) {
       list = specificInformation.value;
     }
 
+    // If the searchValue is null (no search/filtering), display the complete list of items.
     if (searchValue.value === null) {
       list = listValue.value;
     }
@@ -116,11 +203,27 @@ export const useListPage = (specificInformation, searchValue, listValue) => {
   };
 };
 
-export const useSelectedRecord = (searchContents, form, recordProperty) => {
+/**
+ * useSelectedRecord
+ *
+ * Returns the selected record from the search results based on the record property.
+ *
+ * @param {ref} search - The ref containing the search results.
+ * @param {ref} form - The ref containing the form data.
+ * @param {string} recordProperty - The property used to compare the selected record.
+ * @returns {Object} An object with a computed property 'selectedRecord' representing the matched record.
+ */
+export const useSelectedRecord = (search, form, recordProperty) => {
   const selectedRecord = computed(() => {
-    return searchContents.value.find(
-      (record) => record[recordProperty] === form.value[recordProperty]
-    );
+    // If there are search results and the search results are an array, find the matching record based on the 'recordProperty'.
+    if (search.value && Array.isArray(search.value)) {
+      return search.value.find(
+        (record) => record[recordProperty] === form.value[recordProperty]
+      );
+    } else {
+      // If there are no search results or the search results are not an array, return null.
+      return null;
+    }
   });
 
   return {
